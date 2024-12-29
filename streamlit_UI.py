fa34de7d5234dbe67dba4f8c7f9fdcb217cda692
Parker <wilsam2NE1@gmail.com>
2024-12-29 17:05:02 +0530

    Update streamlit_UI.py
    I have made some changes to the file and essentially to the interface. I have added the youtube video title.

--- streamlit_UI.py
+++ streamlit_UI.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from timestamps import processed_timestamps
-from pyperclip import copy
+import pyperclip
+
 # Center the title
 st.markdown("""
     <style>
@@ -18,6 +19,12 @@ st.markdown("""
         margin: 0 auto;
         display: block;
     }
+    .video-title {
+        text-align: center;
+        font-size: 2.5em;
+        font-weight: bold;
+        margin-top: 20px;
+    }
     </style>
 """, unsafe_allow_html=True)
 
@@ -27,14 +34,20 @@ st.markdown('<div class="title">YouTube Timestamps Extractor</div>', unsafe_allo
 # Add a wide text input bar for the YouTube link
 link = st.text_input("", placeholder="Paste YouTube link here", label_visibility="hidden")
 
+output_text = ""
+
 # Add a button below the text input
 if st.button("Get Timestamps!"):
     if link.strip():
         st.write(f"Processing timestamps for: {link}")
-        timestamps = processed_timestamps(link)
+        title, timestamps = processed_timestamps(link)
+
+        # Display the video title in bold and larger font size
+        st.markdown(f'<div class="video-title">{title}</div>', unsafe_allow_html=True)
 
         for data in timestamps:
+            output_text += f"{data} : {timestamps[data]}\n"
             st.write(f"{data} : {timestamps[data]}")
-            
+        
     else:
         st.error("Please paste a valid YouTube link.")
